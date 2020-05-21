7fb37fef53129efe4b339fbf30af9c9c29110344
kenendo12 <kenendo12@gmail.com>
2020-05-21 12:25:22 -0500

    adds dom tutorial and assignment

---
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+        public TodoItemInfo TodoItemInfo { get; set; }
+    }
+    public class TodoItemInfo
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public long TodoItemId { get; set; }
+        public TodoItem TodoItem { get; set; }
+    }
+}
